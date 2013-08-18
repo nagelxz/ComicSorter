70a0850102d08c5fbd53b77257df49a1c6ae4dcb
Jon Nagel <j.nagelxz@gmail.com>
2013-08-17 23:43:48 -0400

    First push containing the C# code. So far its only half of the add folders program.
    Its amazing at the difference in changing languages does. The code is almost half the size compared to Java, granted I probably didn't optimize the code as well as I could of.
    
    Being that Java is the first language I learned, and I'm still learning it, it's a lot faster to write than C#. I haven't done anything in C# for 2 years since I touched Silverlight for an internship, where I didn't do anything with Directory or file manipulations so i have to do a lot of Googling and probably finding the most streamlined way to do things.

---
+++ C#/ComicSorter_v2/ComicSorter_v2/AddNewFolders.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ComicSorter_v2
+{
+     public class AddNewFolders
+     {
+          public String directory = @"F:\Wolfpack\Comics\";
+          public String comics2add = "comics2add.txt";
+          public String comicsList = "comics list.txt";
+
+          public List<String> comicsToAdd;
+          public List<String> comics;
+
+          public static void Main(string[] args)
+          {
+               AddNewFolders anf = new AddNewFolders();
+              
+               List<String> directories = new List<String>(Directory.EnumerateDirectories(anf.directory));      
+               anf.comicsToAdd = new List<String>(File.ReadAllLines(anf.directory + anf.comics2add));
+               anf.comics = new List<String>(File.ReadAllLines(anf.directory + anf.comics));
+          }
+
+          private void mkDirs(String[] directories)
+          {
+               for (int i = 0; i < comicsToAdd.Count; i++)
+               {
+                    if(!Directory.Exists(directory + comicsToAdd[i]))
+                    {
+                         Console.WriteLine(comicsToAdd[i]);
+                         Directory.CreateDirectory(directory + comicsToAdd[i]);
+                    }
+               }
+          }
+
+          private void repopComicsList()
+          {
+               for (int i = 0; i < comicsToAdd.Count; i++)
+               {
+                    if (!comics.Contains(comicsToAdd[i], StringComparer.OrdinalIgnoreCase))
+                    {
+                         comics.Add(comicsToAdd[i]);
+                         Console.WriteLine(comicsToAdd[i] + "added to the comics list.");
+                         comics.Sort();
+                    }
+               }
+          }
+
+          private void updateComicsList()
+          {
+
+          }
+
+          private void resetNewComicsList()//might be able to merge this with updateComicsList
+          {
+
+          }
+     }
+}
