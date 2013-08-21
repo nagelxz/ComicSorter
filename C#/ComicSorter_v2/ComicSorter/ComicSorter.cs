8fc0fbb00e3bc14202c2b6c887b00623560e7e9a
Jon Nagel <j.nagelxz@gmail.com>
2013-08-21 09:35:22 -0400

    Got most of the rest of the code converted. Major parts still missing.
    The parts i have left are a little more than just looking at the Java and converting it over. The regex part needs to be tested before going any farther. Next then is the massive googling for the file manipulations. I'm sure they are easy in comparison to Java, probably as simple as File.Copy and File.Rename but until I'm sure of that I get no joy in swinging a club blindly.

--- C#/ComicSorter_v2/ComicSorter/ComicSorter.cs
+++ C#/ComicSorter_v2/ComicSorter/ComicSorter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace ComicSorter
 {
@@ -39,6 +40,34 @@ namespace ComicSorter
           }
 
           private void nameStripping(String path)
+          {
+               List<String> newName = new List<String>();
+
+               for (int i = 0; i < originalFiles.Count;  )
+               {
+                    newName[i] = originalFiles[i].Replace('_', ' ');
+               }
+
+               String wasteRegex = ".*?(\\()(2)(0)(\\d)(\\d)(\\))";//takes the front of the file name up to the issue year
+                               //this ^ is basically skip
+
+               for (int i = 0; i < newName.Count; i++)
+               {
+                    Regex rgx = new Regex(wasteRegex);
+                    String[] result = rgx.Split(newName[i]);
+                    String fileExt = newName[i].Substring(newName.Count - 4);//Once the initial rewrite is finished change so that the list of filenames are checked before worked on so that they get removed prior to finding out they have the wron extension
+                    newName[i] = newName[i].Replace(result[1], fileExt);//Dont thing this is going to work the same as in Java. While at Newark work test against static string to get this to work before moving testing against any files.
+               }
+
+               for (int i = 0; i < newName.Count; i++)
+               {
+                    //Look into renaming a file in C#, running out of time on the train. Mabe could get lucky and have time to work on it in NYC.
+               }
+
+               sort(path);
+          }
+
+          private void sort(String Path)//Same thing goes for here, i need to look more indepth into how C# handles folder/file manipulation for compying, moving, renaming, deleting before I can go any further. Pulling into Secacus doesn't give me much of that time.
           {
 
           }
--- C#/ComicSorter_v2/ComicSorter/Program.cs
+++ C#/ComicSorter_v2/ComicSorter/Program.cs
@@ -9,6 +9,8 @@ namespace ComicSorter
      {
           static void Main(string[] args)
           {
+               ComicSorter cs = new ComicSorter();
+
                String downloadLocation = @"F:\Downloads\";
                String holdingLocation = @"F:\Wolfpack\Comics\TO BE SORTED INTO RESPECTIVE FOLDERS";//Downloaded from unconventional means.
                String userInput = "";
@@ -16,30 +18,33 @@ namespace ComicSorter
                Console.WriteLine("Where do you want to sort from?  Enter your choice:  ");
                Console.WriteLine("1 \t Downloads folder\n2 \t TO BE SORTED folder\n3 \t Other - manual input location\n");
 
-
                Int32 input = Convert.ToInt32(Console.ReadLine());
 
                switch (input)
                {
                     case 1:
                          //1 downloads folder
-
+                         cs.startingDir = downloadLocation;
                          break;
                     case 2:
                          //2 TO BE SORTED FOLDER
-
+                         cs.startingDir = holdingLocation;
                          break;
                     case 3:
                          //3 other manual input
                          Console.Write("Enter the location you would like to sort from:  ");
                          userInput = Console.ReadLine();
-
+                         cs.startingDir = userInput;
                          break;
                     default:
                          Console.WriteLine("Failed to comply to directions. Terminating Life.");
                          Environment.Exit(0);
                          break;
                }
+
+               cs.folderRecursion(cs.startingDir);
+
+               //I'll write the failed files code once i get the rest working.
 
           }
      }
