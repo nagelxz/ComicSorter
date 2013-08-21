060c4ae7511ab7819942876e785e81b7b6945dc1
Jon Nagel <j.nagelxz@gmail.com>
2013-08-20 21:37:24 -0400

    More work on the main program
    Did a little bit of work while stuck sitting in the computer lab where I work part time. Kind of half-assed at going it. so theres not that much in there.
    
    Ok its a bit of a lie, I got most of the Main file completed and about a 3rd of the class file.

--- C#/ComicSorter_v2/ComicSorter/ComicSorter.cs
+++ C#/ComicSorter_v2/ComicSorter/ComicSorter.cs
@@ -2,11 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace ComicSorter
 {
      class ComicSorter
      {
+          private List<String> directories;
+          private List<String> originalFiles;
 
+          public String startingDir = "";
+
+          public List<String> failed = new List<String>();
+
+          public void folderRecursion(String path)
+          {
+               directories = new List<String>(Directory.EnumerateDirectories(path));
+
+               originalFiles = new List<String>(Directory.EnumerateFiles(path));
+
+               nameStripping(startingDir);
+
+               if (directories.Count == 0)
+               {
+                    originalFiles = new List<String>(Directory.EnumerateFiles(path));
+
+                    nameStripping(path);
+               }
+               else
+               {
+                    foreach(String folder in directories)
+                    {
+                         folderRecursion(path + "\\" + folder);
+                    }
+               }
+          }
+
+          private void nameStripping(String path)
+          {
+
+          }
      }
 }
