6ec92423271bbecec65054aab0d5fde2670c1456
Jon Nagel <j.nagelxz@gmail.com>
2014-05-09 19:43:23 -0400

    woooo it works.
    the new list way is better. Theres still a bit of changes i have to make, that will also make the code a little shorter, but from the looks of it ver 2.1 is done.

--- C#/ComicSorter_v2/ComicSorter/ComicSorterFresh.cs
+++ C#/ComicSorter_v2/ComicSorter/ComicSorterFresh.cs
@@ -16,14 +16,15 @@ namespace ComicSorter
 
           public void Start()
           {
-               folderRecursion(startDir);
+               FolderRecursion(startDir);
                Console.WriteLine("Now that was fun");
                originalFiles = originalFiles.OrderBy(f => f.oFilename).ToList();
-               nameStripping();
+               NameStripping();
                Console.WriteLine("Now that was fun");
+               Sorting();
           }
 
-          private void folderRecursion(String path)
+          private void FolderRecursion(String path)
           {
                List<String> directories = new List<String>(Directory.EnumerateDirectories(path).Select(d => new DirectoryInfo(d).Name));
 
@@ -38,26 +39,16 @@ namespace ComicSorter
                          originalFiles.Add(new FileObject { oFilename = file, fileLocation = path });
                     }
                }
-
-               //if (directories.Count == 0)//might be ble to merge this with the other if statement. LOOK AT LATER!!!
-               //{
-               //     List<Object> temp = new List<Object>(Directory.EnumerateFiles(path).Select(f => Path.GetFileName(f)));
-
-               //     foreach (String file in temp)
-               //     {
-                         
-               //     }
-               //}
                else
                {
                     foreach (String folder in directories)
                     {
-                         folderRecursion(path + folder + "\\");
+                         FolderRecursion(path + folder + "\\");
                     }
                }
           }
 
-          private void nameStripping()
+          private void NameStripping()
           {
                List<String> goodExt = new List<String>(){ "cb7", "cbr", "cbt", "cbz", "pdf" };
                
@@ -68,7 +59,7 @@ namespace ComicSorter
                     file.nFilename = file.oFilename.Replace('_', ' ');
 
                     String wasteRegex = ".*?(\\()(2)(0)(\\d)(\\d)(\\))";//takes the front of the file name up to the issue year
-                    //this ^ is basically skip
+                                   //this ^ is basically skip
 
                     Regex rgx = new Regex(wasteRegex);
                     String result = String.Join("", rgx.Split(file.nFilename));
@@ -77,7 +68,7 @@ namespace ComicSorter
 
                     try//see if i could cut out this move now that i have the new name connected to the old name... will probably make it faster, haha Oh notation hope you see this one day mike.
                     {
-                         File.Move(file.fileLocation + file.oFilename, file.fileLocation + file.nFilename)
+                         File.Move(file.fileLocation + file.oFilename, file.fileLocation + file.nFilename);
                     }
                     catch (FileNotFoundException e)
                     {
@@ -85,6 +76,42 @@ namespace ComicSorter
                     }
                }
 
+          }
+
+          private void Sorting()
+          {
+               String finalDestination = @"C:\temp\Comics\";
+               List<String> comics = new List<String>(Directory.EnumerateDirectories(finalDestination).Select(d => new DirectoryInfo(d).Name));
+
+               foreach (FileObject file in originalFiles)
+               {
+                    Regex rgx = new Regex(".*?(\\s)(\\d)");
+                    String result = String.Join("", rgx.Split(file.nFilename));
+
+                    String comicName = file.nFilename.Substring(0, file.nFilename.Length - result.Length);
+
+                    if (comics.Contains(comicName))
+                    {
+                         Boolean succ = false;
+
+                         try
+                         {
+                              File.Copy(file.fileLocation + file.nFilename, finalDestination + comicName + "\\" + file.nFilename, true);
+                              succ = true;
+                         }
+                         catch (Exception e)
+                         {
+                              Console.WriteLine(e.ToString());
+                              orphanedFiles.Add(new FileObject { oFilename = file.nFilename, fileLocation = file.fileLocation });
+                         }
+
+                         if (succ)
+                         {
+                              Console.WriteLine("File:  " + (file.fileLocation + file.oFilename) + " successfully moved.");
+                              File.Delete(file.fileLocation + file.nFilename);
+                         }
+                    }
+               }
           }
      }
 
